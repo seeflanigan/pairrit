17e2716425965801721ff965150ee68974dbb86d
Cory Flanigan <cory@goldenmeansolutions.com>
2016-09-26 20:43:04 -0600

    Move comments and WIP design notes to their own file

--- spec/pairrit_spec.js
+++ spec/pairrit_spec.js
@@ -1,24 +1,6 @@
 const expect = require('chai').expect;
 const pairrit = require('../lib/pairrit');
 
-// /pair join - no pair name, pair doesn't exist for my username
-// Create a pair named after my username
-//
-// /pair join - no pair name, already in the pair
-// No-op
-//
-// /pair join pairrit - pair name given, pair doesn't exist yet
-// Create a pair named 'pairrit', adds my user to participants
-//
-// /pair join pairrit - pair name given, pair does exist
-// Adds my unsername to participants for 'pairrit'
-//
-// /pair join pairrit - pair name given, already in pairrit
-// no-op
-//
-// /pair join teamBatman - pair name given, you're already in pairrit
-// no-op, vs implicit '/leave'
-
 describe('Pairrit', function() {
   describe('/pair join' , function() {
     it('creates a new pair with the given user and name',  function() {
@@ -84,23 +66,3 @@ describe('Pairrit', function() {
     });
   });
 });
-
-// /pair join #pairrit
-//
-// events:
-//
-// user         #pair-id   action   time
-// -----------------------------------------
-// @adeschamps  #pairrit   join     12:00
-// @jchambers   #pairrit   join     12:01
-// @cflanigan   #pairrit   join     12:12
-
-// if all state is immutable, and the current state is just a projection of state change over time
-// how do we know and display the current state of the application
-// if we are only showing events within 12 hours, we may have a user in a pair who joined > 12 ago, and has not left yet
-
-// events while you were pairing: who is in the pair when you join
-// if you're already in a pair, joining a different pair is an implicit leave (v1)
-// if you're already in a pair, you see buttons "do you want to switch?" 
-
-// * when do we stop showing a pair on the list (eg 12 hours with 0 users)
