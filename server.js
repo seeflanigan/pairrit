6714bb07d82eebedb9702957d5cc4ee8f1303b7f
Cory Flanigan <cory@goldenmeansolutions.com>
2016-10-03 16:30:23 -0600

    Parameterize server port via ENV var

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const router = require('./lib/index');
 
+var serverPort = function () {
+  return process.env.HTTP_PORT || 1337;
+}();
+
 app.get('/', router);
 
-app.listen(1337, function () {
-	console.log('App listening on port 1337');
+app.listen(serverPort, function () {
+  console.log('App listening on port ' + serverPort);
 });
