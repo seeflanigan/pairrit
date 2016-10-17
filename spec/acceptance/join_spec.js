53da78ea45d8358d5b3367b41edad22527e81233
Cory Flanigan <cory@goldenmeansolutions.com>
2016-10-17 17:03:27 -0600

    Update spec to have expected message

--- spec/acceptance/join_spec.js
+++ spec/acceptance/join_spec.js
@@ -2,28 +2,20 @@ const expect  = require('chai').expect;
 const request = require('supertest');
 const td      = require('testdouble');
 
-describe('POST /join', () => {
-  var server;
+const app = require('../../lib/app');
 
-  beforeEach(function () {
-    delete require.cache[require.resolve('./server')];
-    server = require('../../server');
-  });
-
-  afterEach(function (done) {
-    server.close(done);
-  });
-
+describe('POST with `join` command', () => {
   it('accepts content type and returns json', function(done) {
-    request(server)
-      .post('/join')
-      .expect(200, done);
-      // .set('Accept', 'application/json')
-      // .set('Content-Type', 'application/x-www-form-urlencoded')
-      // .end(function(err, res) {
-      //   if (err) return done(err);
-      //   done();
-      // });
+    request(app)
+      .post('/')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.text).to.eql('Welcome to the `batman` pair');
+        done();
+       });
   });
 
   // it('correctly handles complete params', function(done) {
