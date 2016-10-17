a37fe02fff6b42d115c84ef7baa35f4ca443fe8b
Cory Flanigan <cory@goldenmeansolutions.com>
2016-10-17 17:29:16 -0600

    Initial implementation of posting sample join request

--- spec/acceptance/join_spec.js
+++ spec/acceptance/join_spec.js
@@ -6,10 +6,24 @@ const app = require('../../lib/app');
 
 describe('POST with `join` command', () => {
   it('accepts content type and returns json', function(done) {
+    const requestParams = {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'alfred',
+      command:       '/pair',
+      text:          'join',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    };
+
     request(app)
       .post('/')
+      .type('application/x-www-form-urlencoded')
       .set('Accept', 'application/json')
-      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send(requestParams)
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
