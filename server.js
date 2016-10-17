cdc28b32667f0c57a25a71d0e60f2143263fddac
Cory Flanigan <cory@goldenmeansolutions.com>
2016-10-17 14:18:15 -0600

    Separate the application logic from server logic

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const app = express();
-const router = require('./lib/index');
+'use strict';
 
-var serverPort = function () {
+// import our express application
+const app = require('./lib/app');
+
+// assign a server port
+const serverPort = function () {
   return process.env.HTTP_PORT || 1337;
 }();
 
-app.get('/', router);
-
+// set up a listener to serve the application
 const server = app.listen(serverPort, function () {
   console.log('App listening on port %s', server.address().port );
 });
 
 module.exports = server;
+
