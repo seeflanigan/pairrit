52bd093f4bb1855dc01061e0bcd7b3e70cfe19e3
Cory Flanigan <cory@goldenmeansolutions.com>
2016-11-04 16:29:10 -0600

    WIP - add AT for joining and listing pairs

--- spec/acceptance/join_a_pair_spec.js
+++ spec/acceptance/join_a_pair_spec.js
@@ -5,7 +5,7 @@ const td      = require('testdouble');
 const app = require('../../lib/app');
 
 describe('POST with `join` command', () => {
-  it('accepts content type and returns json', function(done) {
+  it('accepts content type and returns json', (done) => {
     const requestParams = {
       token:         'gIkuvaNzQIHg97ATvDxqgjtO',
       team_id:       'T0001',
@@ -32,7 +32,7 @@ describe('POST with `join` command', () => {
        });
   });
 
-  it('respects a channel name option', function(done) {
+  it('respects a channel name option', (done) => {
     const requestParams = {
       token:         'gIkuvaNzQIHg97ATvDxqgjtO',
       team_id:       'T0001',
@@ -57,5 +57,95 @@ describe('POST with `join` command', () => {
         expect(res.text).to.eql('Welcome to the `batcave` pair!');
         done();
       });
+  });
+
+  it.only('adds you to an existing pair', (done) => {
+    const firstJoinParams = {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'alfred',
+      command:       '/pair',
+      text:          'join batcave',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    };
+
+    const secondJoinParams = {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'batman',
+      command:       '/pair',
+      text:          'join batcave',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    };
+
+    const listParams = {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'batman',
+      command:       '/pair',
+      text:          'list',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    };
+
+    request(app)
+      .post('/')
+      .type('application/x-www-form-urlencoded')
+      .set('Accept', 'application/json')
+      .send(firstJoinParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.text).to.eql('Welcome to the `batcave` pair!');
+      });
+
+    request(app)
+      .post('/')
+      .type('application/x-www-form-urlencoded')
+      .set('Accept', 'application/json')
+      .send(secondJoinParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.text).to.eql('Welcome to the `batcave` pair!');
+      });
+
+    const expectedResponse = {
+      "response_type": "in_channel",
+      "text":          "The following pairs are in progress:",
+      "attachments": [{
+        "fallback": "",
+        "title":    "batcave (1 participant)",
+        "text":     "alfred, batman"
+      }]
+    };
+
+    request(app)
+      .post('/')
+      .type('application/x-www-form-urlencoded')
+      .set('Accept', 'application/json')
+      .send(listParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(JSON.parse(res.text)).to.eql(expectedResponse);
+        done();
+      });
+  });
+
+
+  it('implicitly removes you from a pair if you join another', (done) => {
+    done();
   });
 });
--- spec/acceptance/list_pairs_spec.js
+++ spec/acceptance/list_pairs_spec.js
@@ -1,11 +1,11 @@
 const expect  = require('chai').expect;
 const request = require('supertest');
-const td      = require('testdouble');
+const _       = require('lodash/fp');
 
 const app = require('../../lib/app');
 
 describe('Listing Pairs', () => {
-  it('responds with a message if there are no pairs', function(done) {
+  it('responds with a message if there are no pairs', (done) => {
     const listRequestParams = {
       token:         'gIkuvaNzQIHg97ATvDxqgjtO',
       team_id:       'T0001',
@@ -35,7 +35,7 @@ describe('Listing Pairs', () => {
       });
   });
 
-  it('returns a pair with at least one participant', function(done) {
+  it('returns a pair with at least one participant', (done) => {
     // Create a Pair
     const joinRequestParams = {
       token:         'gIkuvaNzQIHg97ATvDxqgjtO',
@@ -100,42 +100,117 @@ describe('Listing Pairs', () => {
        });
   });
 
-  // it('correctly parses the channel name', function(done) {
-  //   const requestParams = {
-  //     token:         'gIkuvaNzQIHg97ATvDxqgjtO',
-  //     team_id:       'T0001',
-  //     team_domain:   'fictional_universe',
-  //     channel_id:    'C2147483705',
-  //     channel_name:  'gotham',
-  //     user_id:       'U2147483697',
-  //     user_name:     'batman',
-  //     command:       '/pair',
-  //     text:          'join batcave',
-  //     response_url:  'https://hooks.slack.com/commands/1234/5678'
-  //   };
+  it.only('only returns the most recent copy of a pair', (done) => {
+    const joinRequestsParams = [{
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'alfred',
+      command:       '/pair',
+      text:          'join batcave',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    }, {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'batwoman',
+      command:       '/pair',
+      text:          'join batcave',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    }, {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'joker',
+      command:       '/pair',
+      text:          'join arkham',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    }, {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'batman',
+      command:       '/pair',
+      text:          'join batcave',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    }, {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'catwoman',
+      command:       '/pair',
+      text:          'join arkham',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    }];
 
-  //   request(app)
-  //     .post('/')
-  //     .type('application/x-www-form-urlencoded')
-  //     .set('Accept', 'application/json')
-  //     .send(requestParams)
-  //     .expect(200)
-  //     .end(function(err, res) {
-  //       if (err) return done(err);
-  //       expect(res.text).to.eql('Welcome to the `batcave` pair!');
-  //       done();
-  //      });
-  // });
+    _.map((joinRequestParams) => {
+      request(app)
+        .post('/')
+        .type('application/x-www-form-urlencoded')
+        .set('Accept', 'application/json')
+        .send(joinRequestParams)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+        });
+    }, joinRequestsParams);
 
-  // it('correctly handles incomplete params', function(done) {
-  //   request(server)
-  //     .post('/join')
-  //     .set('Accept', 'application/json')
-  //     .set('Content-Type', 'application/x-www-form-urlencoded')
-  //     .expect(200)
-  //     .end(function(err, res) {
-  //       if (err) return done(err);
-  //       done();
-  //     });
-  // });
+    // List Pairs
+    const listRequestParams = {
+      token:         'gIkuvaNzQIHg97ATvDxqgjtO',
+      team_id:       'T0001',
+      team_domain:   'fictional_universe',
+      channel_id:    'C2147483705',
+      channel_name:  'gotham',
+      user_id:       'U2147483697',
+      user_name:     'bruce',
+      command:       '/pair',
+      text:          'list',
+      response_url:  'https://hooks.slack.com/commands/1234/5678'
+    };
+
+    const expectedPairs = [{
+      fallback: '',
+      title: 'batcave (3 participants)',
+      text: 'alfred, batwoman, batman'
+    }, {
+      fallback: '',
+      title: 'arkham (2 participants)',
+      text: 'joker, catwoman'
+    }]
+
+    const expectedResponse = {
+      response_type: 'in_channel',
+      text: 'The following pairs are in progress:',
+      attachments: expectedPairs
+    };
+
+    // exercise our behavior
+    request(app)
+      .post('/')
+      .type('application/x-www-form-urlencoded')
+      .set('Accept', 'application/json')
+      .send(listRequestParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.text).to.eql(JSON.stringify(expectedResponse));
+        done();
+       });
+  });
 });
