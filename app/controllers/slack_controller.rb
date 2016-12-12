0b29683f82df57c4904e13c3dd7e5c4d95e480cd
Jonmichael Chambers <jon@tyrbo.net>
2016-12-12 10:31:00 -0700

    wip

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,12 +1,16 @@
 require 'httparty'
 
 class SlackController < ApplicationController
+#{"ok"=>true,
+# "access_token"=>
+#  "xoxp-12460568371-12462919158-110074413927-565cc3f8d16a348450354ce3ac331fe4",
+# "scope"=>"identify,commands",
+# "team_name"=>"Personal",
+# "team_id"=>"T0CDJGQAX"}
   def create
     result = Slack.new(params['code']).verify
 
-    pp result
-
-    render json: {}
+    render json: { ok: true }
   end
 
   def handle
